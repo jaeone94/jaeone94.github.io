4e54724279f14fae6c0f337fac7c781b9fbe342f
jaeone94 <jaeone.prt@gmail.com>
2021-08-31 00:33:16 +0900

    2021-08-30 post minor modification2

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -12,7 +12,7 @@ var store = [{
         "teaser": "/assets/images/config/default_teaser.png"
       },{
         "title": "[node.js] express 프레임워크 설치",
-        "excerpt":"인프런 제로쵸님의 “[리뉴얼] Node.js 교과서 - 기본부터 프로젝트 실습까지” 를 수강후기 포스트입니다. Express.js 프레임워크란 ? Node.js를 위한 웹 프레임워크의 하나로 자바스크립트 런타임 환경에서 웹 어플리케이션을 편하게 개발하기 위한 각종 라이브러리와 미들웨어들이 기본 내장되어 있고 프레임워크로서 개발 규칙을 강제하여 코드 구조를 간결하고 통일성있게 유지시켜주는 도구이다. (Express는 유연한 편이다.) Node.js 웹 프레임워크는...","categories": ["Node.js"],
+        "excerpt":"인프런 제로쵸님의 “[리뉴얼] Node.js 교과서 - 기본부터 프로젝트 실습까지” 를 수강하고 배운내용을 기록한 포스트입니다. Express.js 프레임워크란 ? Node.js를 위한 웹 프레임워크의 하나로 자바스크립트 런타임 환경에서 웹 어플리케이션을 편하게 개발하기 위한 각종 라이브러리와 미들웨어들이 기본 내장되어 있고 프레임워크로서 개발 규칙을 강제하여 코드 구조를 간결하고 통일성있게 유지시켜주는 도구이다. (Express는 유연한 편이다.) Node.js...","categories": ["Node.js"],
         "tags": ["Node.js","express"],
         "url": "/node.js/Node.js-Express-%ED%94%84%EB%A0%88%EC%9E%84%EC%9B%8C%ED%81%AC-%EC%84%A4%EC%B9%98/",
         "teaser": "/assets/images/config/default_teaser.png"
@@ -24,7 +24,7 @@ var store = [{
         "teaser": "/assets/images/config/default_teaser.png"
       },{
         "title": "[node.js] express 프레임워크의 미들웨어",
-        "excerpt":"인프런 제로쵸님의 “[리뉴얼] Node.js 교과서 - 기본부터 프로젝트 실습까지” 를 수강후기 포스트입니다. 미들웨어란 ? 익스프레스는 가볍고 유연한 웹 프레임워크인데, 이 것은 미들웨어 구조 덕분에 가능한 것이다. 미들웨어는 어플리케이션의 호출과 응답 사이, 즉 중간(middle)에 위치해 요청을 처리하기 때문에 미들웨어라고 한다. 미들웨어는 req, res, next를 매개변수로 가진 함수이다. req, res에 접근해 요청과...","categories": ["Node.js"],
+        "excerpt":"인프런 제로쵸님의 “[리뉴얼] Node.js 교과서 - 기본부터 프로젝트 실습까지” 를 수강하고 배운내용을 기록한 포스트입니다. 미들웨어란 ? 익스프레스는 가볍고 유연한 웹 프레임워크인데, 이 것은 미들웨어 구조 덕분에 가능한 것이다. 미들웨어는 어플리케이션의 호출과 응답 사이, 즉 중간(middle)에 위치해 요청을 처리하기 때문에 미들웨어라고 한다. 미들웨어는 req, res, next를 매개변수로 가진 함수이다. req, res에...","categories": ["Node.js"],
         "tags": ["Node.js","express"],
         "url": "/node.js/Node.js-Express-%ED%94%84%EB%A0%88%EC%9E%84%EC%9B%8C%ED%81%AC%EC%9D%98-%EB%AF%B8%EB%93%A4%EC%9B%A8%EC%96%B4/",
         "teaser": "/assets/images/config/default_teaser.png"
