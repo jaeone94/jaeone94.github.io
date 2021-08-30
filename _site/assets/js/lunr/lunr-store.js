71f31a3da5d8daf6b7be8b53e7e2a9bfb8659fa9
jaeone94 <jaeone.prt@gmail.com>
2021-08-31 00:18:58 +0900

    2021-08-30 post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -22,4 +22,10 @@ var store = [{
         "tags": [],
         "url": "/%EC%9E%A1%EC%83%9D%EA%B0%81/%EB%A9%94%ED%83%80%EB%B2%84%EC%8A%A4%EC%97%90-%EB%8C%80%ED%95%9C-%EC%83%9D%EA%B0%81/",
         "teaser": "/assets/images/config/default_teaser.png"
+      },{
+        "title": "[node.js] express 프레임워크의 미들웨어",
+        "excerpt":"인프런 제로쵸님의 “[리뉴얼] Node.js 교과서 - 기본부터 프로젝트 실습까지” 를 수강후기 포스트입니다. 미들웨어란 ? 익스프레스는 가볍고 유연한 웹 프레임워크인데, 이 것은 미들웨어 구조 덕분에 가능한 것이다. 미들웨어는 어플리케이션의 호출과 응답 사이, 즉 중간(middle)에 위치해 요청을 처리하기 때문에 미들웨어라고 한다. 미들웨어는 req, res, next를 매개변수로 가진 함수이다. req, res에 접근해 요청과...","categories": ["Node.js"],
+        "tags": ["Node.js","express"],
+        "url": "/node.js/Node.js-Express-%ED%94%84%EB%A0%88%EC%9E%84%EC%9B%8C%ED%81%AC%EC%9D%98-%EB%AF%B8%EB%93%A4%EC%9B%A8%EC%96%B4/",
+        "teaser": "/assets/images/config/default_teaser.png"
       }]
