da5fdffa9a5504f0b924b0f6a96d1af53486d3bd
jaeone94 <jaeone.prt@gmail.com>
2021-09-11 01:00:39 +0900

    wpqkf

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210911.0056';
+const cacheName = 'chirpy-20210911.0100';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
