1f92c4272e02f95248ed8350630b238e7bbbdc37
jaeone94 <jaeone.prt@gmail.com>
2021-09-11 01:03:45 +0900

    test

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210911.0100';
+const cacheName = 'chirpy-20210911.0102';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
